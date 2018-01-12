790b49a4dd05b55443e7a2d35b5cd4019e2b4ab7
Tom Clark <thc29@MacBook-Pro-2.local>
2018-01-12 17:07:03 +0000

    Append input text with a delimiter to make inputs consisting of solely a date work

--- datefinder.py
+++ datefinder.py
@@ -145,6 +145,9 @@ class DateFinder(object):
 
     def find_dates(self, text, source=False, index=False, strict=False, vector=False):
 
+        # Append text with a delimiter to make inputs consisting of solely a date work
+        text = text + ' '
+
         for date_string, indices, captures in self.extract_date_strings(text, strict=strict):
 
             as_dt_or_vec = self.parse_date_string(date_string, captures, vector)
